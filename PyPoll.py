df02228b9c8a0630d05d7337798a86fc88e8ab7f
ajmotylinski <drew.motylinski@gmail.com>
2022-01-03 11:48:29 -0600

    Print out Election Summary

--- PyPoll.py
+++ PyPoll.py
@@ -16,6 +16,11 @@ candidate_options= []
 #Dictionary for votes for each candidate
 candidate_votes = {}
 
+#Winning candidate information
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -50,4 +55,16 @@ with open(file_to_load) as election_data:
         #Calculate percentage based on votes and total_votes
         vote_percentage = float(votes) / float(total_votes) *100
 
-        print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+    winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+    print(winning_candidate_summary)
